0d235fb9b603cbe30e2d9eb4c76e30ba22fb8561
matanrotgolz <88656761+matanrotgolz@users.noreply.github.com>
2022-01-10 19:53:11 +0200

    Update htmlAttacment.py

--- htmlAttacment.py
+++ htmlAttacment.py
@@ -1,25 +1,25 @@
 
 import updatingContent
 def dayone(name):
-    subject = "Why it can be Hard to Lose Weight"
+    subject = "Subject Example1"
     dayoneText ="   <p> Hi there, "+str.title(name)+", this is Gidon. <br>"+updatingContent.updating('updatecontentday1')+".</p>"
             
     return dayoneText,subject
 
 def daytwo(name):
-    subject = "  Targeting the Root Cause of Gaining Weight!"
+    subject = "Subject Example2"
     dayTowtext= " Hi there, "+str.title(name)+",<p> "+updatingContent.updating('updatingcontentday2')+".</p>"
                  
     return dayTowtext,subject
 
 
 def daythree(name):
-    subject = "  Foods and Herbs to Lose Weight! "
+    subject = " Subject Example3 "
     dayThreeText = "  <p>Hi there, "+str.title(name)+","+updatingContent.updating('updatingcontentday3')+".</p>"
                      
     return dayThreeText,subject
 
-def dayfour(name ):
-    subject = "Spices and Fruits to Lose Weight! "
+def dayfour(name):
+    subject = "Subject Example4 "
     dayFourText = "  <p>Hi there ,"+str.title(name)+",</p>"+"<br><br><p>"+updatingContent.updating('updatingcontentday4')+".</p>"
     return dayFourText,subject
